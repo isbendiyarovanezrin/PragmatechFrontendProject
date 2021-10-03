c9572259651cec8e97830f93fe9120192d76aec1
Nəzrin <isbndyrva@gmail.com>
2021-10-03 13:04:51 -0700

    modal image

---
+++ Extra/modal image/app.js
@@ -0,0 +1,13 @@
+let img = document.getElementById("myImg");
+let modal = document.getElementById("myModal");
+let modalImg = document.getElementById("modalImg");
+
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+};
+
+let span = document.getElementsByClassName("close")[0];
+span.onclick = function () {
+  modal.style.display = "none";
+};
