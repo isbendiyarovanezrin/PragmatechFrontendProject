7f7cfa1d87158347e865a65214a6d41add77dfb9
Nəzrin <isbndyrva@gmail.com>
2021-10-09 18:13:12 +0400

    js practice

---
+++ Javascript/exercises/3/app.js
@@ -0,0 +1,13 @@
+// n tam ədədi verilir. Onun müsbət, mənfi və ya 0-a bərabər olmasını müəyyənləşdirin. n-in qiymətindən asılı olaraq "Positive", "Negative" və ya "Zero" verməli.
+let n = 2001;
+
+if (n == 0) {
+  document.write("The number is ZERO");
+} else if (n > 0) {
+  document.write("The number is POSITIVE");
+} else {
+  document.write("The number is NEGATIVE");
+}
+
+// müsbət, mənfi və ya 0-a bərabər olmasını aşağıdakı kimi də yoxlamaq olar
+document.write(Math.sign(n));
