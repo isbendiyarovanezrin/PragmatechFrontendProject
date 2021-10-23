9dec5fe128ea411353abc289e10583411d41ca3a
Nəzrin <isbndyrva@gmail.com>
2021-10-23 15:03:17 +0400

    random background color

---
+++ Javascript/exercises/9/app.js
@@ -0,0 +1,10 @@
+function changeColor() {
+  document.body.style.backgroundColor =
+    "rgb(" +
+    Math.floor(Math.random() * 285) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    ")";
+}
