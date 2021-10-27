b097c645d1b2908faf4d8c5aa5da73eba5ea595e
Nəzrin <isbndyrva@gmail.com>
2021-10-28 03:05:03 +0400

    js - practice

--- Javascript/exercises/on/app.js
+++ Javascript/exercises/on/app.js
@@ -6,6 +6,12 @@ let findLetter = names.filter((name) => name.match(/[o]/gi));
 newArr.push(findLetter);
 console.log(newArr);
 
+// daxilinde "e" herfi olan telebelerin adlarini bir arraya yigin
+let _NewArr = [];
+let _findLetter = names.filter((name) => name.match(/[e]/gi));
+_NewArr.push(_findLetter);
+console.log(_NewArr);
+
 // adinin uzunlugu 5-den boyuk olan telebelerin adlarini bir arraya yigin
 let NewArr = [];
 let lenNames = names.filter((name) => name.length > 5);
