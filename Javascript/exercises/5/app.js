4ab556920f53a569e2766edb9f8768c25abf5f2e
Nəzrin <isbndyrva@gmail.com>
2021-10-28 22:00:39 +0400

    js - array

--- Javascript/exercises/5/app.js
+++ Javascript/exercises/5/app.js
@@ -1,11 +1,16 @@
 // massivdəki sondan 4 elementin cəmini tapın
 let arr = [13, 46, 59, 88, 40, 40, 4, 6, 8];
 
-n = 4;
-cut = arr.slice(-n);
+let n = 4;
+let cut = arr.slice(-n);
 
 function getSum(total, num) {
   return total + num;
 }
 
-document.write(cut.reduce(getSum));
+document.write(cut.reduce(getSum) + `<br>`);
+
+// massivdəki əvvəldən 2 elementin cəmini tapın
+let two = arr.slice(0, 2);
+
+document.write(two.reduce(getSum));
