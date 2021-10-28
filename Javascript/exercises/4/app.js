6f6887fc0a045a1299d274d8b31d6d1be4d42fb6
Nəzrin <isbndyrva@gmail.com>
2021-10-28 22:17:12 +0400

    js - array

--- Javascript/exercises/4/app.js
+++ Javascript/exercises/4/app.js
@@ -2,10 +2,10 @@ let arr = [1, 23, 33, 55, 67, 276, 5, 9, 58, 46];
 
 // massivdəki tək ədəd sayını tapın
 let oddNum = arr.filter((n) => n % 2 !== 0);
-oddLength = oddNum.length;
+let oddLength = oddNum.length;
 document.write(oddLength + "<br>");
 
 // massivdəki cüt ədəd sayını tapın
 let evenNum = arr.filter((n) => n % 2 == 0);
-evenLength = evenNum.length;
+let evenLength = evenNum.length;
 document.write(evenLength);
