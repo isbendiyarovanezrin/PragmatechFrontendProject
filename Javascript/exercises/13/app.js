0ded481ec97f312c05ccb03b7af2d99852a20f71
Nəzrin <isbndyrva@gmail.com>
2021-10-28 21:28:02 +0400

    creating javascript random numbers with Math.random()

--- Javascript/exercises/13/app.js
+++ Javascript/exercises/13/app.js
@@ -7,3 +7,12 @@ function randomNumber(n) {
   let _random = Math.floor(Math.random() * n);
   return _random;
 }
+
+let message = document.getElementById("message");
+
+message.innerHTML = RandomNumber(300);
+
+function RandomNumber(num) {
+  let Random = Math.round(Math.random() * num);
+  return Random;
+}
