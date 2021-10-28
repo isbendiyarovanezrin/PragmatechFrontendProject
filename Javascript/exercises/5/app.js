8c71ce585e992584f259496760690571935f8400
Nəzrin <isbndyrva@gmail.com>
2021-10-28 22:12:08 +0400

    js - array

--- Javascript/exercises/5/app.js
+++ Javascript/exercises/5/app.js
@@ -13,4 +13,10 @@ document.write(cut.reduce(getSum) + `<br>`);
 // massivdəki əvvəldən 2 elementin cəmini tapın
 let two = arr.slice(0, 2);
 
-document.write(two.reduce(getSum));
+document.write(two.reduce(getSum) + `<br>`);
+
+// massivdəki 4-cü sırada duran element ilə 6-cı sırada duran elementin cəmini tapın
+let num1 = arr[3];
+let num2 = arr[5];
+
+document.write(num1 + num2);
