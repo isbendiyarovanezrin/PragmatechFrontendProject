b9d592136cc2f30acf75a52d52fdc09e9a0d0d2d
Nəzrin <isbndyrva@gmail.com>
2021-10-28 18:10:23 +0400

    js - for loop

---
+++ Javascript/exercises/12/app.js
@@ -0,0 +1,22 @@
+/* 
+Bunu yaratdım.
+
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+**********
+
+*/
+
+for (let column = 1; column <= 10; column++) {
+  for (let row = 1; row <= column; row++) {
+    document.write(`*`);
+  }
+  document.write(`<br>`);
+}
