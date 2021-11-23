0689862262b76846820107560304b06b5fdb41e2
Nəzrin <isbndyrva@gmail.com>
2021-11-23 12:36:59 +0400

    Add modal box

---
+++ Extra/modalbox/js/modal.js
@@ -0,0 +1,11 @@
+let btn = document.getElementById("btn");
+let modal = document.getElementById("modal");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
