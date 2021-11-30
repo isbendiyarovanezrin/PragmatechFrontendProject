f4e1042fc91400f1efbbb2b2d81ab5d45c76ef8c
Nəzrin <isbndyrva@gmail.com>
2021-11-30 15:26:21 +0400

    Add typing effect to text

---
+++ Javascript/projects/typing effect/js/app.js
@@ -0,0 +1,10 @@
+let i = 0;
+let text = "Mən əla yazılıram!";
+
+function typingEffect() {
+  if (i < text.length) {
+    document.getElementById("text").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typingEffect, 60);
+  }
+}
