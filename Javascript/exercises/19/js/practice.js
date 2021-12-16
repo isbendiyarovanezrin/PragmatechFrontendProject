4fb65458f3e97a84339b4d47f5fd586d8fa0b3b2
Nəzrin <isbndyrva@gmail.com>
2021-12-16 18:28:24 +0400

    Add practice with objects

--- Javascript/exercises/19/js/practice.js
+++ Javascript/exercises/19/js/practice.js
@@ -1,7 +1,7 @@
 // practice
 
 // 1
-let user = {
+let user1 = {
   name: "Nazrin",
   age: 17,
 };
@@ -16,8 +16,8 @@ let user3 = {
   age: 55,
 };
 
-Object.assign(user, user2, user3);
-console.log(user); // { name: 'Ali', age: 55 }
+Object.assign(user1, user2, user3);
+console.log(user1); // { name: 'Ali', age: 55 }
 
 // 2
 let fruit = {
@@ -30,3 +30,27 @@ let energy = {
 
 Object.assign(fruit, energy);
 console.log(fruit); // { name: 'apple', calorie: 95 }
+
+// 3
+let obj1 = {
+  surname: "Memmedov",
+};
+
+let obj2 = Object.assign({}, obj1);
+console.log(obj1.surname); // Memmedov
+console.log(obj2.surname); // Memmedov
+
+// 4
+// Write the code, one line for each action:
+
+// 1. Create an empty object user.
+// 2. Add the property name with the value John.
+// 3. Add the property surname with the value Smith.
+// 4. Change the value of the name to Pete.
+// 5. Remove the property name from the object.
+
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
