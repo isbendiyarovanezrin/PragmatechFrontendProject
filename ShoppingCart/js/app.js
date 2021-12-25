cf1b70a538d7cc015be52f5c0d0dbef64ac287a1
Nəzrin <isbndyrva@gmail.com>
2021-12-25 20:06:15 +0400

    Update ShoppingCart project

--- ShoppingCart/js/app.js
+++ ShoppingCart/js/app.js
@@ -1,21 +1,40 @@
-// const productList = document.querySelector(".product-list");
+const productList = document.querySelector(".product-list");
 
-// function loadJson() {
-//   fetch("data/products.json")
-//     .then((responce) => responce.json())
-//     .then((data) => {
-//       let item = "";
-//       data.forEach((product) => {
-//         item += `
-//         `;
-//       });
-//       productList.innerHTML = item;
-//     })
-//     .catch((error) => {
-//       alert(error);
-//     });
-// }
-// loadJson();
+function loadJson() {
+  fetch("data/products.json")
+    .then((responce) => responce.json())
+    .then((data) => {
+      let item = "";
+      data.forEach((product) => {
+        item += `
+        <div class="col-3 product-item" data="${product.id}">
+           <div class="product-img-box">
+             <img src="${product.imgSrc}" alt="product image" class="product-img" />
+             <div class="overlay">
+               <a class="overlay-link" href="product.html?id=${product.id}">
+                 <img src="images/arrow.png" alt="arrow" class="arrow" />
+               </a>
+               <div class="overlay-info">
+                 <p class="overlay-content-p">Design | Branding</p>
+                          <h2 class="text-center">Creative Web Design</h2>
+               </div>
+             </div>
+           </div>
+           <hr />
+           <div class="product-content">
+             <p class="product-price">${product.price}</p>
+             <button type="button" class="btn-add-cart">+ Cart</button>
+           </div>
+       </div>
+        `;
+      });
+      productList.innerHTML = item;
+    })
+    .catch((error) => {
+      alert(error);
+    });
+}
+loadJson();
 
 // loader
 function removeLoader() {
