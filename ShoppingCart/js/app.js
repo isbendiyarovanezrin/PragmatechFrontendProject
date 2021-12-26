ab210e1df77c6b36d6b8ea030e0760f57907acd7
Nəzrin <isbndyrva@gmail.com>
2021-12-26 20:24:12 +0400

    Update ShoppingCart project

--- ShoppingCart/js/app.js
+++ ShoppingCart/js/app.js
@@ -1,32 +1,18 @@
-const productList = document.querySelector(".product-list");
+let productList = document.querySelector(".product-list");
 
 function loadJson() {
   fetch("data/products.json")
     .then((responce) => responce.json())
     .then((data) => {
+      let categories = document.querySelector(".filters");
       let item = "";
-      data.forEach((product) => {
-        item += `
-        <div class="col-3 product-item" data="${product.id}">
-           <div class="product-img-box">
-             <img src="${product.imgSrc}" alt="product image" class="product-img" />
-             <div class="overlay">
-               <a class="overlay-link" href="product.html?id=${product.id}">
-                 <img src="images/arrow.png" alt="arrow" class="arrow" />
-               </a>
-               <div class="overlay-info">
-                 <p class="overlay-content-p">Design | Branding</p>
-                          <h2 class="text-center">Creative Web Design</h2>
-               </div>
-             </div>
-           </div>
-           <hr />
-           <div class="product-content">
-             <p class="product-price">${product.price}</p>
-             <button type="button" class="btn-add-cart">+ Cart</button>
-           </div>
-       </div>
-        `;
+      data.forEach((element) => {
+        if (element.filter_name == "All categories") {
+          element.products.forEach((element) => {
+            item += productView(element);
+          });
+        }
+        categories.innerHTML += categoryView(element);
       });
       productList.innerHTML = item;
     })
@@ -35,6 +21,56 @@ function loadJson() {
     });
 }
 loadJson();
+
+function categoryView(category) {
+  return `
+  <button class="filter-option" onclick="categoryFilter(${category.filter_id})">${category.filter_name}</button>
+  `;
+}
+
+function productView(product) {
+  return `
+  <div class="col-3 product-item" data="${product.id}">
+     <div class="product-img-box">
+       <img src="${product.imgSrc}" alt="product image" class="product-img" />
+       <div class="overlay">
+         <a class="overlay-link" href="product.html?id=${product.id}">
+           <img src="images/arrow.png" alt="arrow" class="arrow" />
+         </a>
+         <div class="overlay-info">
+           <p class="overlay-content-p">Design | Branding</p>
+           <h2 class="text-center">Creative Web Design</h2>
+         </div>
+       </div>
+       <p class="num-box"></p>
+     </div>
+     <hr />
+     <div class="product-content">
+       <p class="product-price">${product.price}</p>
+       <button type="button" class="btn btn-add-cart"><span class="add-btn">+</span>&nbsp;Cart</button>
+     </div>
+ </div>
+  `;
+}
+
+function categoryFilter(id) {
+  fetch("data/products.json")
+    .then((responce) => responce.json())
+    .then((data) => {
+      let products = document.querySelector(".products");
+      products.innerHTML = "";
+      for (let category of data) {
+        if (category.filter_id == id) {
+          category.products.forEach((element) => {
+            products.innerHTML += productView(element);
+          });
+        }
+      }
+    })
+    .catch((error) => {
+      alert(error);
+    });
+}
 
 // loader
 function removeLoader() {
