52db15a6639c698f101e5f40c3b606aa83c00671
Nəzrin <isbndyrva@gmail.com>
2022-01-09 22:24:42 +0400

    Update JavaScript mousemove project

--- Javascript/projects/mousemove/js/app.js
+++ Javascript/projects/mousemove/js/app.js
@@ -1,8 +1,12 @@
+"use strict";
+
 let cursor = document.getElementById("cursor");
 
-window.addEventListener("mousemove", function (e) {
+window.addEventListener("mousemove", changeCursor);
+
+function changeCursor(e) {
   let x = e.clientX;
   let y = e.clientY;
   cursor.style.left = `${x}px`;
   cursor.style.top = `${y}px`;
-});
+}
