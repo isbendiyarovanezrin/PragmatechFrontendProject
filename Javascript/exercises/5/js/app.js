deb8e06369c737a9e345f9df6a68247bfa83545e
Nəzrin <isbndyrva@gmail.com>
2022-01-31 00:09:53 +0400

    Update array practice

--- Javascript/exercises/5/js/app.js
+++ Javascript/exercises/5/js/app.js
@@ -1,8 +1,8 @@
 // massivdəki sondan 4 elementin cəmini tapın
-let arr = [13, 46, 59, 88, 40, 40, 4, 6, 8];
+const arr = [13, 46, 59, 88, 40, 40, 4, 6, 8];
 
-let n = 4;
-let cut = arr.slice(-n);
+const n = 4;
+const cut = arr.slice(-n);
 
 function getSum(total, num) {
   return total + num;
@@ -11,12 +11,12 @@ function getSum(total, num) {
 document.write(cut.reduce(getSum) + `<br>`);
 
 // massivdəki əvvəldən 2 elementin cəmini tapın
-let two = arr.slice(0, 2);
+const two = arr.slice(0, 2);
 
 document.write(two.reduce(getSum) + `<br>`);
 
 // massivdəki 4-cü sırada duran element ilə 6-cı sırada duran elementin cəmini tapın
-let num1 = arr[3];
-let num2 = arr[5];
+const num1 = arr[3];
+const num2 = arr[5];
 
 document.write(num1 + num2);
