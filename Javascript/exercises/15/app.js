294fb48612cf5aa125f855db533ecf0179c2b3a4
Nəzrin <isbndyrva@gmail.com>
2022-01-31 02:40:44 +0400

    Update exercise

--- Javascript/exercises/15/app.js
+++
@@ -1,9 +0,0 @@
-let btn = document.querySelector("#create");
-let _div = document.querySelector("#add");
-
-btn.onclick = () => {
-  let div = document.createElement("div");
-  div.textContent = "Mən gəldim!";
-  div.classList = "box";
-  _div.appendChild(div);
-};
---
+++ Javascript/exercises/15/js/app.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("#create-btn");
+const addBox = document.querySelector("#boxes");
+
+btn.onclick = () => {
+  const div = document.createElement("div");
+  div.textContent = "Mən gəldim!";
+  div.classList = "box";
+  addBox.appendChild(div);
+};
