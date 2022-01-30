a83fa3b2aba2f5468780879fded52656abdaa135
Nəzrin <isbndyrva@gmail.com>
2022-01-31 00:25:31 +0400

    Update array practice

--- Javascript/exercises/7/js/app.js
+++ Javascript/exercises/7/js/app.js
@@ -3,7 +3,7 @@ let arr = ["Aysel", "Guler", "Samir", "Kamil", "Ayla", "Ali", "Mehemmed"];
 // Massivdəki sözlərin hər birinin hərf sayını ekrana çap edin
 for (let i = 0; i < arr.length; i++) {
   word = arr[i];
-  document.write(word.length + "<br>");
+  document.write(`${word.length}<br>`);
 }
 
 //Massiv daxilindəki sözlərdə olan "e" hərflərini "ə" ilə əvəz edin
@@ -11,26 +11,26 @@ for (let i = 0; i < arr.length; i++) {
   word = arr[i];
   arr[i] = word.replaceAll(/e/gi, "ə");
 }
-document.write(arr + "<br>");
+document.write(`${arr}<br>`);
 
 //Massiv daxilindəki sözlərdə olan "a" hərflərini "o" ilə əvəz edin
 for (let i = 0; i < arr.length; i++) {
   word = arr[i];
   arr[i] = word.replaceAll(/a/gi, "o");
 }
-document.write(arr + "<br>");
+document.write(`${arr}<br>`);
 
 // Massivdəki sondan 3 sözü ekrana yazdırın
 lastWord = arr.slice(-3);
-document.write(lastWord + "<br>");
+document.write(`${lastWord}<br>`);
 
 // Massivdə başdan birinci sözü ekrana yazdırın
 firstWord = arr.slice(0)[0];
-document.write(firstWord + "<br>");
+document.write(`${firstWord}<br>`);
 
 // Massivdə başdan ikinci sözü ekrana yazdırın
 secondWord = arr.slice(1)[0];
-document.write(secondWord + "<br>");
+document.write(`${secondWord}<br>`);
 
 // Massivdəki ilk iki sözdən başqa qalan bütün sözləri ekrana yazdırın
 _words = arr.splice(2);
