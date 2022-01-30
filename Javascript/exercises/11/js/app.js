28f63702e6688252f6a5d5198d895ebb8bce3799
Nəzrin <isbndyrva@gmail.com>
2022-01-31 01:52:14 +0400

    Update exercise

--- Javascript/exercises/11/js/app.js
+++ Javascript/exercises/11/js/app.js
@@ -1,8 +1,8 @@
 function calculator() {
-  let number1 = parseInt(document.getElementById("number1").value);
-  let number2 = parseInt(document.getElementById("number2").value);
-  let operation = document.getElementById("operation").value;
-  let result = document.getElementById("result");
+  const number1 = parseInt(document.getElementById("number1").value);
+  const number2 = parseInt(document.getElementById("number2").value);
+  const operation = document.getElementById("operation").value;
+  const result = document.getElementById("result");
 
   if (operation == "+") {
     result.value = number1 + number2;
