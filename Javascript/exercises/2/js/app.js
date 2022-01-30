a6d574478f6e72ecdafddccd43f8892424c8049c
Nəzrin <isbndyrva@gmail.com>
2022-01-30 23:57:38 +0400

    Add window prompt() method practice

--- Javascript/exercises/2/js/app.js
+++ Javascript/exercises/2/js/app.js
@@ -1,14 +1,9 @@
 // Kvadrat yaratmaq üçün daxil edilən 4 ədəddən ekrana kvadratın sahəsini, olmazsa, həmin ədədlərin cəmini yazın.
 
-let num1 = prompt("birinci ədədi daxil edin");
-let num2 = prompt("ikinci ədədi daxil edin");
-let num3 = prompt("üçüncü ədədi daxil edin");
-let num4 = prompt("dördüncü ədədi daxil edin");
-
-num1 = Number(num1);
-num2 = Number(num2);
-num3 = Number(num3);
-num4 = Number(num4);
+const num1 = Number(prompt("Birinci ədədi daxil edin"));
+const num2 = Number(prompt("İkinci ədədi daxil edin"));
+const num3 = Number(prompt("Üçüncü ədədi daxil edin"));
+const num4 = Number(prompt("Dördüncü ədədi daxil edin"));
 
 if (num1 == num2 && num1 == num3 && num1 == num4) {
   alert("Kvadratın sahəsi: " + Math.pow(num1, 2)); // kvadratın sahəsi: S= a*a
