b58e0e9ef9bd097fe69e2bd1f1b57a6a462ec8a8
Nəzrin <isbndyrva@gmail.com>
2022-02-11 14:12:45 +0400

    Update react project

--- React/practices/src/components/Navbar.js
+++ React/practices/src/components/Navbar.js
@@ -140,6 +140,9 @@ class Navbar extends Component {
                   className="btn btn-outline-success"
                   type="submit"
                   style={btnStyle}
+                  onClick={(e) => {
+                    e.preventDefault();
+                  }}
                 >
                   Search
                 </button>
