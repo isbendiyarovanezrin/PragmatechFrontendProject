d8fadac2c8d18a740127eae74a490fd0380539f2
Nəzrin <isbndyrva@gmail.com>
2022-03-05 16:42:41 +0400

    Update app.js

--- Music Balloons/js/app.js
+++ Music Balloons/js/app.js
@@ -9,6 +9,19 @@ window.addEventListener("keydown", function (e) {
   key.classList.add("playing");
 });
 
+window.addEventListener("click", (e) => {
+  const sounds = document.querySelectorAll("audio[data-key]");
+  sounds.forEach((sound) => {
+    if (
+      e.target.getAttribute("data-key") == sound.getAttribute("data-key") ||
+      e.target.innerHTML.toLowerCase() == sound.getAttribute("data-key")
+    ) {
+      sound.currentTime = 0;
+      sound.play();
+    }
+  });
+});
+
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeStyle));
 
